fc0b04798af892c7ed892a561b47be24a36862f3
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-22 15:29:20 -0600

    Remove unnecessary connection to db

--- config/connection.js
+++ config/connection.js
@@ -9,14 +9,5 @@ var connection = mysql.createConnection({
     database: process.env.DB || "burgers_db"
 });
 
-// connect to the database
-connection.connect(function(error) {
-    if (error) {
-        console.error("error connecting: " + error.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
 // export the connection
 module.exports = connection;
