0362a069f397901eb9357c276ea190985279a8dc
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-23 17:46:54 -0600

    Update to use connection pooling for mysql

--- config/connection.js
+++ config/connection.js
@@ -10,12 +10,7 @@ var dbConfig = {
 
 // setup the connection
 // DATABASE_URL env is set up for heroku
-var connection = mysql.createConnection(process.env.DATABASE_URL || dbConfig);
-
-// app is crashing on heroku due to idle timeout. This keeps a connection open
-setInterval(function() {
-    connection.query('SELECT 1');
-}, 5000);
+var connection = mysql.createPool(process.env.DATABASE_URL || dbConfig);
 
 // export the connection
 module.exports = connection;
