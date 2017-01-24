ff8aedfc7b4569da2126d3292e387cea4b372e1d
Christopher Brenner <christopher.brenner1@northwestern.edu>
2017-01-23 21:50:02 -0600

    Update promises to callbacks (#2)
    * Update to use connection pooling for mysql
    
    * Update promises to callbacks

--- config/connection.js
+++ config/connection.js
@@ -10,12 +10,8 @@ var dbConfig = {
 
 // setup the connection
 // DATABASE_URL env is set up for heroku
-var connection = mysql.createConnection(process.env.DATABASE_URL || dbConfig);
-
-// app is crashing on heroku due to idle timeout. This keeps a connection open
-setInterval(function() {
-    connection.query('SELECT 1');
-}, 5000);
+// using pooling to stop app from crashing
+var connection = mysql.createPool(process.env.DATABASE_URL || dbConfig);
 
 // export the connection
 module.exports = connection;
