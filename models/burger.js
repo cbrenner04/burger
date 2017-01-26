d02559427a3521c442d833fb8a31d3f46f650ad0
cbrenner04 <christopher.brenner1@northwestern.edu>
2017-01-25 20:11:09 -0600

    Add ORM

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,12 @@
-// require the database connection
-var connection = require('../config/connection.js');
+// require the orm
+var orm = require('../config/orm.js');
 
 // export the model
 module.exports = {
-    allBurgers: burgers,
-    create: create,
-    singleBurger: singleBurger,
-    update: update
+    allBurgers   : burgers,
+    create       : create,
+    singleBurger : singleBurger,
+    update       : update
 };
 
 // get all the burgers -- an object is returned with uneaten and eaten burgers
@@ -23,46 +23,35 @@ function burgers(callBack) {
 
 // query the database for uneaten burgers
 function uneatenBurgers(callBack) {
-    connection.query('SELECT * FROM burgers WHERE devoured = false',
-        function(error, data) {
-            if (error) throw error;
-            callBack(data);
-        });
+    orm.selectScoped('burgers', 'devoured', 'false', function(data) {
+        callBack(data);
+    });
 }
 
 // query the database for eaten burgers
 function eatenBurgers(callBack) {
-    connection.query('SELECT * FROM burgers WHERE devoured = true',
-        function(error, data) {
-            if (error) throw error;
-            callBack(data);
-        });
+    orm.selectScoped('burgers', 'devoured', 'true', function(data) {
+        callBack(data);
+    });
 }
 
 // query the database for single burger
 function singleBurger(burger, callBack) {
-    connection.query('SELECT * FROM burgers WHERE id = ?', [burger],
-        function(error, data) {
-            if (error) throw error;
-            callBack(data);
-        });
+    orm.selectOne('burgers', burger, function(data) {
+        callBack(data);
+    });
 }
 
 // query the database to create a burger
 function create(burger, callBack) {
-    connection.query("INSERT INTO burgers (burger_name) VALUES (?)", [burger],
-        function(error, result) {
-            if (error) throw error;
-            callBack();
-        });
+    orm.insertOne('burgers', 'burger_name', burger, function() {
+        callBack();
+    });
 }
 
 // query the database to update a burger
 function update(burger, callBack) {
-    connection
-        .query("UPDATE burgers SET devoured = true WHERE id = ?", [burger],
-            function(error, result) {
-                if (error) throw error;
-                callBack();
-            });
+    orm.updateOne('burgers', 'devoured', true, burger, function() {
+        callBack();
+    });
 }
